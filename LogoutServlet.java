f3ce9797df63f470461dbb43fc45837f388d7ef6
Damindu Sandaruwan <damindusandaruwan001@gmail.com>
2025-05-02 15:34:51 +0530

    Update LogoutServlet.java

--- LogoutServlet.java
+++ LogoutServlet.java
@@ -5,11 +5,30 @@ import javax.servlet.http.*;
 
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO: Implement logout logic
-        // 1. Invalidate session
+
+        // 1. Invalidate the session
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
         // 2. Remove username cookie
-        // 3. Redirect to login.html
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("username".equals(cookie.getName())) {
+                    cookie.setValue("");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
+
+        // 3. Redirect to login page
+        response.sendRedirect("login.html");
     }
 }
+
+        
