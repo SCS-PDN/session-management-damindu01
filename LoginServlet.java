be579fefbde70fab72277f2cb14f593ae590b00a
Damindu Sandaruwan <damindusandaruwan001@gmail.com>
2025-05-02 15:34:09 +0530

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,19 +1,14 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-
-@WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        // TODO: Implement login logic
-        // 1. Get username & password from request
-        // 2. Validate credentials (hardcode a few users)
-        // 3. If valid:
-        //    - Create session
-        //    - Store username in cookie
-        //    - Redirect to DashboardServlet
-        // 4. If invalid, redirect back to login.html
-    }
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>University Course Registration - Login</title>
+</head>
+<body>
+    <h1>Login to Course Registration</h1>
+    <form action="LoginServlet" method="POST">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
